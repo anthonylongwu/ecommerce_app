4f3cf3ac5d7a969a829a55db0f197dd175346099
Anthony Wu <anthonylongwu19@gmail.com>
2015-11-12 21:28:04 -0800

    made more routes, got 6/7 rests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,38 @@
 class ProductsController < ApplicationController
+  
   def index
     @products = Product.all
-
   end
 
+  def new
+  end
+  
+  def create
+    name = params[:name]
+    price = params[:price]
+    image = params[:image]
+    description = params[:description]
+    Product.create(name: name, price: price, image: image, description: description)
+  end
+
+
+  def show
+    id = params[:id]
+    @product = Product.find_by(id: id)
+  end
+
+    def edit
+    id = params[:id]
+    @product = Product.find_by(id: id)
+    end 
+
+    def update
+      id = params[:id]
+      product = Product.find_by(id: id)
+      name = params[:name]
+      price = params[:price]
+      image = params[:image]
+      description = params[:description]
+      Product.update(name: name, price: price, image: image, desctiption: description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  get '/index', to: 'products#index'
+  get '/', to: 'products#index'
+  get '/products/new', to: 'products#new'
+  post '/products', to: 'products#create'
+  post '/create', to: 'products#create'
+  get '/products/:id', to: 'products#show'
+  get '/products/:id/edit', to: 'products#edit'
+  patch '/products/:id', to: 'products#update'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
